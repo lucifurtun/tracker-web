0dd892418f090692386f4848f80798ccfcbaf848
Luci Furtun <lucianfurtun@gmail.com>
2017-11-25 22:59:39 +0200

    Set the correct title

--- src/app/layout/layout-routing.module.ts
+++ src/app/layout/layout-routing.module.ts
@@ -13,7 +13,6 @@ const routes: Routes = [
             {path: 'tables', loadChildren: './tables/tables.module#TablesModule'},
             {path: 'forms', loadChildren: './form/form.module#FormModule'},
             {path: 'bs-element', loadChildren: './bs-element/bs-element.module#BsElementModule'},
-            {path: 'grid', loadChildren: './grid/grid.module#GridModule'},
             {path: 'components', loadChildren: './bs-component/bs-component.module#BsComponentModule'},
             {path: 'blank-page', loadChildren: './blank-page/blank-page.module#BlankPageModule'}
         ]
@@ -22,7 +21,7 @@ const routes: Routes = [
         path: '',
         component: LayoutComponent,
         children: [
-            {path: '', redirectTo: 'dashboard'},
+            {path: '', redirectTo: 'devices'},
             {path: 'positions', loadChildren: './position/position.module#PositionModule'},
             {path: 'devices', loadChildren: './device/device.module#DeviceModule'},
         ]
